0613d348f92db0b30d1f4907d867e7dbd5ae297f
whitewuwukai <guoliang@wustl.edu>
2023-10-05 06:14:11 +0800

    flag

--- src/studio4/Flag.java
+++ src/studio4/Flag.java
@@ -7,5 +7,17 @@ import edu.princeton.cs.introcs.StdDraw;
 public class Flag {
 	public static void main(String[] args) {
 		
+		StdDraw.setPenRadius(0.05);
+		StdDraw.point(0.5, 0.5);
+		
+		StdDraw.setPenColor(255, 255, 255);
+        StdDraw.filledRectangle(3,3,3,4);
+        
+        StdDraw.point(7, 7);
+        
+        StdDraw.setPenColor(233, 0, 0);
+        StdDraw.filledCircle(0.5,.5,0.3);
+        StdDraw.show();
+		
 	}
 }
--- src/studio4/InterpretDrawingFile.java
+++ src/studio4/InterpretDrawingFile.java
@@ -20,5 +20,66 @@ public class InterpretDrawingFile {
 		File f = new File(chooser.getSelectedFile().getPath());
 		Scanner in = new Scanner(f); //making Scanner with a File
 		
+		String type = in.nextLine();
+		
+		if (type == "rectangle") {
+			int redComponent = in.nextInt();
+			int greenComponent = in.nextInt();
+			int blueComponent = in.nextInt();
+			boolean isFilled = in.nextBoolean();
+			double x = in.nextDouble();
+			double y = in.nextDouble();
+			double halfWidth = in.nextDouble();
+			double halfHeight = in.nextDouble();
+			
+			
+			if (isFilled) {
+				StdDraw.setPenRadius(0.05);
+				StdDraw.point(0.5, 0.5);
+				
+				StdDraw.setPenColor(redComponent, greenComponent, blueComponent);
+		        StdDraw.rectangle(x, y, halfWidth, halfHeight);
+		        StdDraw.show();
+			}
+			
+			else {
+				StdDraw.setPenRadius(0.05);
+				StdDraw.point(0.5, 0.5);
+				
+				StdDraw.setPenColor(redComponent, greenComponent, blueComponent);
+		        StdDraw.rectangle(x, y, halfWidth, halfHeight);
+		        StdDraw.show();
+			}
+		
+	        
+		}
+		//if (type == "ellipse") {
+			
+			
+			//double x = in.nextDouble();
+			//double y = in.nextDouble();
+			//double semiMajorAxis = in.nextDouble();
+			//double semiMinorAxis = in.nextDouble();
+			
+			//StdDraw.setPenRadius(0.05);
+	        //StdDraw.setPenColor(StdDraw.BLUE);
+	        //StdDraw.point(0.5, 0.5);
+	        //StdDraw.setPenColor(StdDraw.MAGENTA);
+	        //StdDraw.ellipse(x, y, semiMajorAxis, semiMinorAxis);
+		//}
+		//if (type == "triangle") {
+			//double x1 = in.nextDouble();
+			//double y1 = in.nextDouble();
+			//double x2 = in.nextDouble();
+			//double y2 = in.nextDouble();
+			//double x3 = in.nextDouble();
+			//double y3 = in.nextDouble();
+			//StdDraw.setPenRadius(0.05);
+	        //StdDraw.setPenColor(StdDraw.BLUE);
+	        //StdDraw.point(0.5, 0.5);
+	        //StdDraw.setPenColor(StdDraw.MAGENTA);
+	  //      StdDraw.triangle(x1, y1, x2, y2, x3, y3);
+		//}
+		
 	}
 }
